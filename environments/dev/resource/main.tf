c265124c8a7fc83f3d94239da513c711a39799f7
mimant <mimant10@gmail.com>
2024-08-06 16:05:14 +0000

    added commint

--- environments/dev/resource/main.tf
+++ environments/dev/resource/main.tf
@@ -16,7 +16,7 @@ provider "google" {
   project = "${var.project_id}"
 }
 
-module "google_bigquery_dataset" "datasets" {
+resource "google_bigquery_dataset" "datasets" {
   for_each = local.datasets
 
   project                     = var.project_id
@@ -27,7 +27,7 @@ module "google_bigquery_dataset" "datasets" {
 
 }
 
-module "google_bigquery_table" "tables" {
+resource "google_bigquery_table" "tables" {
   for_each = local.tables
 
   project                     = var.project_id
